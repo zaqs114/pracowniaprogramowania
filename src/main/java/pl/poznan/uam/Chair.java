866007f24eafd106bf749dfac0c17593e5a945b9
zaqs114 <zaqs114@gmail.com>
2018-02-13 22:49:33 +0100

    added database classes

---
+++ src/main/java/pl/poznan/uam/Chair.java
@@ -0,0 +1,12 @@
+package pl.poznan.uam;
+
+import lombok.Data;
+
+public @Data
+class Chair {
+    private int id;
+    private int height;
+    private int width;
+    private String color;
+    private String name;
+}
---
+++ src/main/java/pl/poznan/uam/Sets.java
@@ -0,0 +1,4 @@
+package pl.poznan.uam;
+
+public class Sets {
+}
